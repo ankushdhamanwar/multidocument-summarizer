dece047c127f58b38e2f371802df0a31e1af3824
Ankush Dhamanwar <31895895+ankushdhamanwar@users.noreply.github.com>
2019-05-30 22:53:54 +0530

    Update SentenceSimilarity.py

--- Classes/SentenceSimilarity.py
+++ Classes/SentenceSimilarity.py
@@ -2,19 +2,13 @@ import numpy as np
 
 from Classes.WordSimilarity import word_similarity
 
-#from pywsd import disambiguate
-#from pywsd.lesk import simple_lesk
-#from pywsd.similarity import max_similarity
 
 CONST_GAMMA = 1.8
 CONST_PHI = 0.2
 
 
-def sentence_similarity(q1_toks, q2_toks):
-    #q1 = " ".join(q1_toks)
-    #q2 = " ".join(q2_toks)
-    # sense1 = disambiguate(q1)
-    # sense2 = disambiguate(q2)
+def sentence_similarity(dict1, q1_toks, dict2, q2_toks):
+
     max_len = max(len(q1_toks), len(q2_toks))
     sem_vector_v1 = np.zeros(max_len)
     sem_vector_v2 = np.zeros(max_len)
@@ -25,9 +19,14 @@ def sentence_similarity(q1_toks, q2_toks):
     sim = 0
     max_sim = 0
     for word1 in q1_toks:
+        w1 = dict1[word1]
         max_sim = 0
         for word2 in q2_toks:
-            sim = word_similarity(word1, word2)
+            if word1 == word2:
+                max_sim = 1
+                continue
+            w2 = dict2[word2]
+            sim = word_similarity(w1, w2)
             max_sim = max(max_sim, sim)
         if max_sim < CONST_PHI:
             max_sim = 0
@@ -35,13 +34,18 @@ def sentence_similarity(q1_toks, q2_toks):
         i1 = i1 + 1
         if max_sim > 0.8025:
             c1 = c1 + 1
-    #print(sem_vector_v1, c1)
+    print(sem_vector_v1, c1)
     sim = 0
     max_sim = 0
     for word1 in q2_toks:
+        w1 = dict2[word1]
         max_sim = 0
         for word2 in q1_toks:
-            sim = word_similarity(word1, word2)
+            if word1 == word2:
+                max_sim = 1
+                continue
+            w2 = dict1[word2]
+            sim = word_similarity(w1, w2)
             max_sim = max(max_sim, sim)
         if max_sim < CONST_PHI:
             max_sim = 0
@@ -49,12 +53,12 @@ def sentence_similarity(q1_toks, q2_toks):
         i2 = i2 + 1
         if max_sim > 0.8025:
             c2 = c2 + 1
-    #print(sem_vector_v2, c2)
+    print(sem_vector_v2, c2)
     c = (c1 + c2) / CONST_GAMMA
 
     if c == 0:
         c = max_len / 2
-    #print(c)
+    print(c)
     s = (np.linalg.norm(sem_vector_v1) * np.linalg.norm(sem_vector_v2))
     #print(s)
     sim = s / c
